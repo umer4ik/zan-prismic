278dd9d3b41e52a108bce0bf39e780241739521b
umer <umer.hurramov@gmail.com>
2025-07-04 14:21:32 +0300

    sadf

--- src/lib/components/PostFooter.svelte
+++ src/lib/components/PostFooter.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { isArabic } from "$lib/is-arabic";
+  import { isArabic } from '$lib/is-arabic';
 
   let { locale } = $props();
   const titles = {
@@ -7,23 +7,27 @@
     designedBy: 'Designed by',
     tos: 'Terms',
     pp: 'Privacy Policy',
-  }
+  };
   const arabic = isArabic(locale);
   if (arabic) {
-    titles.brand = ' جميع الحقوق محفوظة.'
-    titles.tos = 'الشروط'
-    titles.pp = 'سياسة الخصوصية'
-    titles.designedBy = 'صممه'
+    titles.brand = ' جميع الحقوق محفوظة.';
+    titles.tos = 'الشروط';
+    titles.pp = 'سياسة الخصوصية';
+    titles.designedBy = 'صممه';
   }
 </script>
 
 <div class="post-footer">
   <div class="post-footer__row">
-    <div class="post-footer__copyright" style="{arabic ? 'direction: ltr; text-align: end;' : ''}">
-      <span>© 2025 Zan Agency.</span> <span  style="direction: {arabic ? 'rtl' : 'ltr'}">{titles.brand}</span>
+    <div class="post-footer__copyright" style={arabic ? 'direction: ltr; text-align: end;' : ''}>
+      <span>© 2025 Zan Agency.</span> <span style="direction: {arabic ? 'rtl' : 'ltr'}">{titles.brand}</span>
     </div>
     <div class="post-footer__tos-links">
-      <a href="#">{titles.tos}</a>, <a href="#">{titles.pp}</a>
+      {#if arabic}
+        <a href="#">الشروط</a>، <a href="#">{titles.pp}</a>
+      {:else}
+        <a href="#">{titles.tos}</a>, <a href="#">{titles.pp}</a>
+      {/if}
     </div>
     <div class="post-footer__obys">
       {titles.designedBy} <a target="_blank" href="https://obys.agency/">Obys</a>
